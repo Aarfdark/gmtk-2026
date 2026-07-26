924e081a16d7deefe72bb0ec1899c94769faff7d
Aarfdark <aadarsh.natarajan@gmail.com>
2026-07-25 23:38:38 -0400

    art and upgrade stuff

--- src/dialogue_box/dialogue_box.gd
+++ src/dialogue_box/dialogue_box.gd
@@ -14,18 +14,22 @@ var num_dialogue: int = 0
 var cur_line: String
 
 var dialogue_queue: Array[Dialog] = [] # Likely should be updated so dialog is a custome resource
+
+
 func _ready() -> void:
 	typing_timer.timeout.connect(_on_typing_timer_timeout)
 	add_child(typing_timer)
 
-	dialog_pause.one_shot=true
-	
+	dialog_pause.one_shot = true
+
 	dialog_pause.timeout.connect(play_next_line)
 	add_child(dialog_pause)
+
 
 func init_game_state(gs: GameState) -> void:
 	gs.dialog_reached.connect(queue_dialog)
 	gs.check_dialog()
+
 
 func queue_dialog(to_queue: Dialog) -> void:
 	print(to_queue)
@@ -33,6 +37,7 @@ func queue_dialog(to_queue: Dialog) -> void:
 	if typing_timer.is_stopped() and dialog_pause.is_stopped():
 		play_next_dialog()
 		print("play")
+
 
 func display_text(line: String) -> void:
 	text = line
@@ -48,7 +53,6 @@ func _on_typing_timer_timeout() -> void:
 		dialog_pause.start(dialog_pause_time)
 
 
-
 func play_next_line() -> void:
 	if num_dialogue == current_dialogue.size():
 		play_next_dialog()
@@ -56,20 +60,20 @@ func play_next_line() -> void:
 	cur_line = current_dialogue.get(num_dialogue)
 	display_text(cur_line)
 	num_dialogue += 1
+
+
 func play_next_dialog() -> void:
 	if dialogue_queue.is_empty():
 		return
 	var d: Dialog = dialogue_queue.pop_front()
 	current_dialogue = d.lines.split("\n")
 	num_dialogue = 0
-	
 
 
 func _input(event: InputEvent) -> void:
 	if not ProjectSettings.get_setting("custom/enable_debug_keybinds"):
 		return
 	if event.is_action_pressed("DEBUG_increment_sands"):
-		
 		pass
 # add to queue -> if paused then play_next_dialog
 # dialog start -> play line -> next line[loop] -> play_next_dialog
