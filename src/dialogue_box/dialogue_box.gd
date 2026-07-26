948e1e8255999b255f227e998f6bbe06419a9f1c
VernalUmbrella <44485518+VernalUmbrella@users.noreply.github.com>
2026-07-26 03:03:30 -0400

    Rework dialogue box: button to advance

--- src/dialogue_box/dialogue_box.gd
+++ src/dialogue_box/dialogue_box.gd
@@ -3,8 +3,7 @@ extends RichTextLabel
 
 var current_dialogue: PackedStringArray
 
-var typing_timer: Timer = Timer.new()
-@export var typing_speed: float = 0.03
+@export var advance_button: Button
 
 # var dialog_pause: Timer = Timer.new()
 # @export var dialog_pause_time: float = 1.0
@@ -13,10 +12,11 @@ var cur_line: String
 
 var dialogue_queue: Array[Dialog] = []
 
+@onready var typing_timer: Timer = $TypingTimer
+
 
 func _ready() -> void:
-	typing_timer.timeout.connect(_on_typing_timer_timeout)
-	add_child(typing_timer)
+	advance_button.pressed.connect(advance)
 
 	# dialog_pause.one_shot=true
 
@@ -30,6 +30,7 @@ func init_game_state(gs: GameState) -> void:
 
 func queue_dialog(to_queue: Dialog) -> void:
 	dialogue_queue.append(to_queue)
+	advance_button.disabled = false
 	if is_dialog_completed():
 		play_next_dialog()
 
@@ -41,14 +42,21 @@ func is_dialog_completed() -> bool:
 func display_text(line: String) -> void:
 	text = line
 	visible_characters = 0
-	typing_timer.start(typing_speed)
+	typing_timer.start()
 
 
 func _on_typing_timer_timeout() -> void:
 	if visible_characters < cur_line.length():
 		visible_characters += 1
 	else:
-		typing_timer.stop()
+		finish_typing()
+
+
+func finish_typing() -> void:
+	typing_timer.stop()
+	advance_button.disabled = (
+		dialogue_queue.is_empty() and num_dialogue == current_dialogue.size()
+	)
 
 
 func play_next_line() -> void:
@@ -69,13 +77,17 @@ func play_next_dialog() -> void:
 	play_next_line()
 
 
+func advance() -> void:
+	if typing_timer.is_stopped():
+		play_next_line()
+	else:
+		finish_typing()
+		visible_ratio = 1.0
+
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("skip_dialog"):
-		if typing_timer.is_stopped():
-			play_next_line()
-		else:
-			typing_timer.stop()
-			visible_ratio = 1.0
+		advance()
 
 # add to queue -> if paused then play_next_dialog
 # dialog start -> play line -> next line[loop] -> play_next_dialog
