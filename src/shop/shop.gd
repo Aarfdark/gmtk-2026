41a67911f24b7efaeb833b19e090157f42736de9
VernalUmbrella <44485518+VernalUmbrella@users.noreply.github.com>
2026-07-26 06:38:17 -0400

    Even funnier reverb

--- src/shop/shop.gd
+++ src/shop/shop.gd
@@ -138,7 +138,7 @@ func _on_buy_button_pressed() -> void:
 		clock_hand.texture = hand_sprites[hand_count]
 		# WARN: magic numbers everywhere
 		AudioServer.get_bus_effect(3, 0).set_pitch_scale(1 - (hand_count * 0.1))
-		AudioServer.get_bus_effect(3, 1).set_wet(hand_count * 0.1)
+		AudioServer.get_bus_effect(3, 1).set_wet(hand_count * 0.2)
 
 	# TODO: fancy confirm animation
 	_selected_button.reparent(displayed_slot)
